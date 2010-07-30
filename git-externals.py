06fc891f95c357248dd1c9e76053cfd805b4bd3b
Henrik Eneroth <henrik.eneroth@gmail.com>
2010-07-30 22:49:51 +0200

    v.01

--- git-externals.py
+++ git-externals.py
@@ -1,56 +1,101 @@
 import sys
 import os
 import re
-from os.path import join, abspath, isdir, dirname, split, exists
-from os 		 import listdir, mkdir, linesep
+from os.path import join, abspath, isdir, dirname, split, exists, isfile
+from os      import listdir, mkdir, linesep
 
 # Find .gitexternals files
 def findGitExternals(dirToCheck):
-	extFiles = []
-	for root, dirs, files in os.walk(dirToCheck):
-	  if '.gitexternals' in files:
-	    extFiles.append(join(root,".gitexternals"))
-	
-	return extFiles;
-	
+  extFiles = []
+  for root, dirs, files in os.walk(dirToCheck):
+    if '.gitexternals' in files:
+      extFiles.append(join(root,".gitexternals"))
+  
+  return extFiles;
+  
 def getRepos(extFiles):
-	repos = {}
+  repos = {}
 
-	for extFile in extFiles:
-		baseDir = split(extFile)
-		fileStream = open(extFile, 'r')
-		for index, line in enumerate(fileStream):
-			line = line.strip()
-						
-			# Check for comments
-			r = line.find('#')
-			p = line.find('//')
-			if line == '' or not(p and r):
-				continue
-				
-			parts = line.split("=")
-			
-			# Check for improperly formated strings
-			if not(len(parts) == 2) or parts[0] == '' or parts[1] == '':
-				exString = 'Line ' + str(index) + ' looks weird: "' + line.replace("\n","") + '" in file: "' + extFile + '". Dunno what to do with it.'
-				raise Exception(exString)			
-			
-			repos[join(baseDir[0], parts[0].strip())] = parts[1].strip()
-	return repos
+  for extFile in extFiles:
+    baseDir = split(extFile)
+    fileStream = open(extFile, 'r')
+    for index, line in enumerate(fileStream):
+      line = line.strip()
+            
+      # Check for comments
+      r = line.find('#')
+      p = line.find('//')
+      if line == '' or not(p and r):
+        continue
+        
+      parts = line.split("=")
+      
+      # Check for improperly formated strings
+      if not(len(parts) == 2) or parts[0] == '' or parts[1] == '':
+        exString = 'Line ' + str(index) + ' looks weird: "' + line.replace("\n","") + '" in file: "' + extFile + '". Dunno what to do with it.'
+        raise Exception(exString)      
+      
+      repos[join(baseDir[0], parts[0].strip())] = parts[1].strip()
+  return repos
 
 def updateGitIgnore(repos):
-	for localFolder in repos.keys():
-		gitIgnoreFile = join(split(localFolder)[0], '.gitignore')
-		
-		# Create .gitignore if it doesn't exist
-		if not(exists(gitIgnoreFile)):
-			fileStream = open(gitIgnoreFile, 'w')
-			fileStream.write('')
+  # Make list of SVN folder names and .gitignore-files
+  gitIgnoreFiles = {}
+  
+  for localFolder in repos.keys():
+    tmp     = split(localFolder)
+    baseDir = tmp[0]
+    name    = tmp[1]
+    gitFile = join(baseDir, '.gitignore')
+    
+    if gitFile in gitIgnoreFiles:
+      gitIgnoreFiles[gitFile].append(name)
+    else:
+      gitIgnoreFiles[gitFile] = [name]
+  
+  # Modify .gitignore files
+  gitExtMark = "# gitexternals"
+  gitExtMessage = gitExtMark + '\n';
+  gitExtMessage += "# The line above is a marker that the git-externals script looks for. DO NOT CHANGE IT!\n"
+  gitExtMessage += "# Everything beneath this section is automatically added by git-externals script\n"
+  gitExtMessage += "# Please add anything else ABOVE this section\n"
+  gitExtMessage += "# -------------------------------------------------------------------------------------"
+  
+  for gitIgnoreFile, subFolders in gitIgnoreFiles.items():
+    # Read file, if it exists
+    toWrite = ''
+    
+    if isfile(gitIgnoreFile):
+      fileStream = open(gitIgnoreFile, 'r')
+      
+      for index, line in enumerate(fileStream):
+        if not(line.strip().find(gitExtMark)):
+          break
+        else:
+          toWrite += line
+      
+    
+    toWrite += gitExtMessage
 
+    for subFolder in subFolders:
+      toWrite += '\n' + subFolder
+
+    fileStream = open(gitIgnoreFile, 'w')
+    fileStream.write(toWrite)  
+
+def updateRepos(repos):
+  for localFolder, remoteFolder in repos.items():
+    if split(localFolder)[1] in sys.argv or not(len(sys.argv)):
+      if isdir(localFolder):
+        os.system('svn cleanup ' + localFolder)
+        os.system('svn update ' + localFolder)
+      else:
+        os.system('svn co ' + remoteFolder + ' ' + localFolder)
+      
 # Default settings
 # ------------------------------------------------------
-startDir = abspath(os.getcwd())					# Start in current directory
-extFiles = findGitExternals(startDir)		# Find the locations of the .gitexternals files
-repos    = getRepos(extFiles)						# Get a dictionary of repositories and their local paths
-print(repos)
+startDir = abspath(os.getcwd())          # Start in current directory
+extFiles = findGitExternals(startDir)    # Find the locations of the .gitexternals files
+repos    = getRepos(extFiles)            # Get a dictionary of repositories and their local paths
 updateGitIgnore(repos)
+updateRepos(repos)
